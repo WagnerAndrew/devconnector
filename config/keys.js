36f4b2134287695c11634e30ba30b1460d26c51e
WagnerAndrew <wagner.andrew.james@gmail.com>
2019-02-26 15:18:51 -0700

    First commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://andrew:Mongodbandrew@awcluster-shard-00-00-qbery.mongodb.net:27017,awcluster-shard-00-01-qbery.mongodb.net:27017,awcluster-shard-00-02-qbery.mongodb.net:27017/test?ssl=true&replicaSet=AWCluster-shard-0&authSource=admin&retryWrites=true"
+};
