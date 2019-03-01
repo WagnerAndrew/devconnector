2519a8542ea5afad3028be4234a0368db9d96ff9
WagnerAndrew <wagner.andrew.james@gmail.com>
2019-02-28 18:35:09 -0700

    Added web token

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
   mongoURI:
-    "mongodb://andrew:Mongodbandrew@awcluster-shard-00-00-qbery.mongodb.net:27017,awcluster-shard-00-01-qbery.mongodb.net:27017,awcluster-shard-00-02-qbery.mongodb.net:27017/test?ssl=true&replicaSet=AWCluster-shard-0&authSource=admin&retryWrites=true"
+    'mongodb://andrew:Mongodbandrew@awcluster-shard-00-00-qbery.mongodb.net:27017,awcluster-shard-00-01-qbery.mongodb.net:27017,awcluster-shard-00-02-qbery.mongodb.net:27017/test?ssl=true&replicaSet=AWCluster-shard-0&authSource=admin&retryWrites=true',
+
+  secretOrKey: 'secret'
 };
