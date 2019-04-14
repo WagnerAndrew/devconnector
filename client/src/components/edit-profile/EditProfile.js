4853624ca1f67bfcc8d9c06e369e05bb7877b3d5
WagnerAndrew <wagner.andrew.james@gmail.com>
2019-04-14 15:14:51 -0600

    Experience and Education displaying

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
@@ -193,8 +193,10 @@ class CreateProfile extends Component {
         <div className='container'>
           <div className='row'>
             <div className='col-md-8 m-auto'>
+              <Link to='/dashboard' className='btn btn-light'>
+                Go Back
+              </Link>
               <h1 className='display-4 text-center'>Edit Profile</h1>
-
               <small className='d-block pb-3'>* = required fields</small>
               <form onSubmit={this.onSubmit}>
                 <TextFieldGroup
@@ -294,14 +296,13 @@ class CreateProfile extends Component {
 }
 
 CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  createProfile: PropTypes.func.isRequired,
-  getCurrentProfile: PropTypes.func.isRequired,
-
   profile: state.profile,
   errors: state.errors
 });
